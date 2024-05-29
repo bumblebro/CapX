882f453425a7308cfc1be66f9ccd9052a21cdf0c
Shreyas M S <shreyasmadappady20@gmail.com>
2024-05-29 23:12:52 +0530

    Commit

---
+++ src/components/Body.tsx
@@ -0,0 +1,13 @@
+function Body() {
+  return (
+    <div className="container mx-auto ">
+      <h1 className="text-4xl pt-[159px]">
+        The trading platform that works as your personal assistant
+      </h1>
+      <h2>Trading made simple and smarter</h2>
+      <button>Join Waitlist</button>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <section>
+      <div className="flex justify-center gap-2 container  mx-auto pt-[3.01rem]">
+        <h1 className="text-[#ED8E36] font-semibold">Home</h1>
+        <h1>Coming Soon</h1>
+      </div>
+    </section>
+  );
+}
+
+export default NavBar;
